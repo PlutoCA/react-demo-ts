e38c460ee74e3a36ccb5f1e390d7f1c20880036a
PlutoCA <1031349211@qq.com>
2019-05-28 19:36:43 +0800

    feat: add sass-resources

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,61 @@
+const { override, fixBabelImports, addWebpackAlias, addLessLoader } = require('customize-cra')
+const path = require('path')
+
+const sassRegex = /\.(scss|sass)$/;
+
 module.exports = function override(config, env) {
     // do stuff with the webpack config...
-    console.log(config, env)
+    // config.output.publicPath = env === 'production' ? '/' : '/'
+    config.resolve = {
+        alias: {
+          '@': path.resolve(__dirname, 'src'),
+        }
+      };
+		const loaders = config.module.rules.find(rule => Array.isArray(rule.oneOf)).oneOf;
+		
+		const sassLoader = loaders.find(option => option.test.toString() === sassRegex.toString())
+		console.log(sassLoader)
+		if (sassLoader) {
+			sassLoader.use.push({
+				loader: require.resolve("sass-resources-loader"),
+				options: {
+					sourceMap: true,
+					resources: [
+						path.resolve(__dirname, "src/asset/sass/variable.scss")
+					]
+				},
+			});
+		}
     return config;
 };
+
+// module.exports = override(
+//     // fixBabelImports('import', {
+//     //   libraryName: 'antd',
+//     //   libraryDirectory: 'es',
+//     //   style: 'css',
+//     // }),
+//     addWebpackAlias({
+//       ['@']: path.resolve(__dirname, 'src')
+//     }),
+//     // addLessLoader({
+//     //     rules: [
+//     //     {
+//     //       test: /\.scss$/,
+//     //       use: [
+//     //         'style-loader',
+//     //         'css-loader',
+//     //         'postcss-loader',
+//     //         'sass-loader',
+//     //         {
+//     //           loader: 'sass-resources-loader',
+//     //           options: {
+//     //             resources: [
+//     //                 path.resolve(__dirname, "src/asset/sass/variable.scss")
+//     //             ]
+//     //           },
+//     //         },
+//     //       ],
+//     //     },
+//     //   ]})
+//   )
